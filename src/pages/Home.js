dd8fc34bc299edcd892dae8083653ed072b354d2
vuewebdev <rybniko@gmail.com>
2023-05-07 12:10:06 +0300

    State Management - useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 export const Home = () => {
-  return <h1>Home</h1>;
+  const [count, setCount] = useState(0);
+  const [incrementBy, setIncrementBy] = useState(1);
+  return (
+    <>
+      <h1>Home</h1>
+      <h2>State Management</h2>
+      <h3>Counter with useState</h3>
+      <label>
+        Increment by:
+        <input
+          type="number"
+          value={incrementBy}
+          onChange={(e) => setIncrementBy(Number(e.target.value))}
+        />
+      </label>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + incrementBy)}>Increment</button>
+    </>
+  );
 };
