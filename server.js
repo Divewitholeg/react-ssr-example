faede034a7ea20463eea94bfd344caa6e148c00f
vuewebdev <rybniko@gmail.com>
2023-05-09 17:57:12 +0300

    SSR with useContext

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import { StaticRouter } from "react-router-dom/server";
 import path from "path";
 import fs from "fs";
 import App from "./src/App";
+import "isomorphic-fetch";
+import { InitialDataContext } from "./src/InitialDataContext";
 
 global.window = {};
 
@@ -36,14 +38,29 @@ app.get("/api/articles", (req, res) => {
   res.json(loadedArticles);
 });
 
-app.get("/*", (req, res) => {
+app.get("/*", async (req, res) => {
   const sheet = new ServerStyleSheet();
-  const reactApp = renderToString(
+  const contextObj = { _isServerSide: true, _requests: [], _data: {} };
+  renderToString(
     sheet.collectStyles(
+      <InitialDataContext.Provider value={contextObj}>
+        <StaticRouter location={req.url}>
+          <App />
+        </StaticRouter>
+      </InitialDataContext.Provider>
+    )
+  );
+
+  await Promise.all(contextObj._requests);
+  contextObj._isServerSide = false;
+  delete contextObj._requests;
+
+  const reactApp = renderToString(
+    <InitialDataContext.Provider value={contextObj}>
       <StaticRouter location={req.url}>
         <App />
       </StaticRouter>
-    )
+    </InitialDataContext.Provider>
   );
 
   const templateFile = path.resolve("./build/index.html");
@@ -52,14 +69,12 @@ app.get("/*", (req, res) => {
       return res.status(500).send(err);
     }
 
-    const articlesString = articles;
-
     return res.send(
       data
         .replace(
           '<div id="root"></div>',
           `<script>window.preloadedArticles = ${JSON.stringify(
-            articlesString
+            contextObj
           )};</script></script><div id="root">${reactApp}</div>`
         )
         .replace("{{ styles }}", sheet.getStyleTags())
